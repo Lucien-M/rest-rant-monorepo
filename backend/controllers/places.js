237b74e5f22ea7e5f9b219bd93a25276195eae7d
Lucien-M <mail@gmail.com>
2023-01-20 19:37:38 -0500

    Activity Completed Successfully

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -4,6 +4,11 @@ const db = require("../models");
 const { Place, Comment, User } = db;
 
 router.post("/", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to add a place" });
+  }
   if (!req.body.pic) {
     req.body.pic = "http://placekitten.com/400/400";
   }
@@ -45,6 +50,11 @@ router.get("/:placeId", async (req, res) => {
 });
 
 router.put("/:placeId", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to edit places" });
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -65,6 +75,11 @@ router.put("/:placeId", async (req, res) => {
 });
 
 router.delete("/:placeId", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to delete places" });
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -135,11 +150,9 @@ router.delete("/:placeId/comments/:commentId", async (req, res) => {
         message: `Could not find comment with id "${commentId}" for place with id "${placeId}"`
       });
     } else if (comment.authorId !== req.currentUser?.userId) {
-      res
-        .status(403)
-        .json({
-          message: `You do not have permission to delete comment "${comment.commentId}"`
-        });
+      res.status(403).json({
+        message: `You do not have permission to delete comment "${comment.commentId}"`
+      });
     } else {
       await comment.destroy();
       res.json(comment);
